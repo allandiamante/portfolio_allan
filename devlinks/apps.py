a90d9d3253e043bf6c9cad6966c40db68ca5e94a
unknown <allandiamantedesouza@gmail.com>
2023-05-15 16:01:26 -0300

    first commit

---
+++ devlinks/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DevlinksConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "devlinks"
---
+++ devlinks/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+
+
+class SignUpView(TemplateView):
+    template_name = "links.html"
