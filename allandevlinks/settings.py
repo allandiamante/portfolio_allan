22a0f34dc9e33ad081298640b509a447d81f7dd7
unknown <allandiamantedesouza@gmail.com>
2023-05-15 18:27:35 -0300

    colocando arq static 12

--- allandevlinks/settings.py
+++ allandevlinks/settings.py
@@ -130,5 +130,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 import os
 if os.environ.get("VERCEL"):
-    STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles'),
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'staticfiles','static')
+    STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles')
